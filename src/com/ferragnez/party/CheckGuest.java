c55365638dda1c1c7c634c1f4f6a0fc98c2d0ae1
michelegelmini <michelegelminivr@gmail.com>
2024-06-07 14:34:52 +0200

    cleaner-code

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -7,26 +7,24 @@ public class CheckGuest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String [] guestList = {"w","Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		String [] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 
+		// scansione input utente
 		String userGuest;
-		
 		Scanner scanner = new Scanner(System.in);	
 		System.out.println("Ciao, come ti chiami?");
 		userGuest = scanner.nextLine();
-		
-		
-		
-		
+		scanner.close();
+			
 		boolean isOnTheList = false;
 		
-		
 		//metodo con for
-		/*
-		 * for (int i = 0; i < guestList.length; i++ ) { if
-		 * (guestList[i].equals(userGuest)) { isOnTheList = true; break; } }
-		 */
 		
+//		
+//		  for (int i = 0; i < guestList.length; i++ ) { if
+//		  (guestList[i].equals(userGuest)) { isOnTheList = true; break; } 
+//		}
+		 
 		
 		//metodo con while
 		
@@ -43,14 +41,6 @@ public class CheckGuest {
 		} else {
 			System.out.println("Il tuo nome non è presente, vattene " + userGuest + "!");
 		}
-		
-		scanner.close();
-		
-		
-		
-		
-		
-	
 
 	}
 	}
